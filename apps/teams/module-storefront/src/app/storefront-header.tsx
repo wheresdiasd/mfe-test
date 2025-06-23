b78d254feb44a6d129c95458471fa9656edd0846
Dias, Diego <diasd@ae.com>
2025-06-22 21:36:12 -0300

    typo

--- apps/teams/module-storefront/src/app/storefront-header.tsx
+++ apps/teams/module-storefront/src/app/storefront-header.tsx
@@ -29,7 +29,7 @@ export default function StorefrontHeader() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
-            <h1 className="text-xl font-semibold text-gray-900">Zephy MFE Test</h1>
+            <h1 className="text-xl font-semibold text-gray-900">Zephyr MFE Test 🦅</h1>
           </div>
           <nav className="flex space-x-8">
             <a
